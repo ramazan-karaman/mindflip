43804d54ef7a838c2ab39b1188a0e7e531cd85e2
ramazan-karaman <krmn47rmzn@gmail.com>
2025-10-20 14:52:13 +0300

    servisler oluşturuldu

--- app/pratik/classic.tsx
+++ app/pratik/classic.tsx
@@ -2,7 +2,7 @@ import { useLocalSearchParams, useNavigation } from 'expo-router';
 import React, { useEffect, useState } from 'react';
 import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
-import { getCards, updateCardSRS } from '../../components/db'; // ../../components/db olmalı, yolu kontrol et
+import { getCards, updateCardSRS } from '../../components/db';
 
 interface Card {
     id: number;
